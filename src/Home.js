1f54c31bef97bba8c8a78452fa3de471a3c475a5
nofeesahdee <nofeesahdee@gmail.com>
2022-01-23 14:35:46 +0100

    update

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
+import { useState } from 'react';
+import Bloglist from './Bloglist'
+
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: "My new website", body: "lorem ipsum...", author: 'mario', id: 1},
+        { title: "Welcome party!", body: "lorem ipsum...", author: 'ninja', id: 2},
+        { title: "Web dev top tips", body: "lorem ipsum...", author: 'mario', id: 3}
+    ])
     return ( 
         <div className="home">
-            <h2>The Homepage</h2>
+            <Bloglist blogs={blogs} title = "All Blogs!"/>
+            <Bloglist blogs={blogs.filter((blog) => blog.author === 'mario')} title = "Marios Blog"/>
         </div>
      );
 }
