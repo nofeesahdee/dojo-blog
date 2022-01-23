8c9fb682f8d3e63a439cb24f325560dec3a6bd8a
nofeesahdee <nofeesahdee@gmail.com>
2022-01-23 15:22:33 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-  // const title = 'Welcome to the new blog'
  
   return (
     <div className="App">
-       <Navbar/>
+      <Navbar/>
       <div className="content">
         <Home />
       </div>
--- src/Home.js
+++ src/Home.js
@@ -8,10 +8,14 @@ const Home = () => {
         { title: "Welcome party!", body: "lorem ipsum...", author: 'ninja', id: 2},
         { title: "Web dev top tips", body: "lorem ipsum...", author: 'mario', id: 3}
     ])
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id)
+        setBlogs(newBlogs)
+    }
     return ( 
         <div className="home">
-            <Bloglist blogs={blogs} title = "All Blogs!"/>
-            <Bloglist blogs={blogs.filter((blog) => blog.author === 'mario')} title = "Marios Blog"/>
+            <Bloglist blogs={blogs} title = "All Blogs!" handleDelete = {handleDelete}/>
         </div>
      );
 }
