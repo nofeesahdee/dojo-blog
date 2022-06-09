72a6630edc45be2c0763d7d28958279f8427a5b6
nofeesahdee <nofeesahdee@gmail.com>
2022-06-09 16:51:49 +0100

    built a full blog with react

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,9 +1,9 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect} from 'react';
 
 const useFetch = (url) =>{
-    const [data, setData] = useState(null)
-    const [isPending, setisPending] = useState(true)
-    const [error, setError] = useState(null)
+    const [data, setData] = useState(null);
+    const [isPending, setisPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         const abortCont = new AbortController();
